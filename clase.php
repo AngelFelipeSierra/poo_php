23c6d90457a6b5210ec4dbaceeeb9ada855eadef
Angel Sierra <ancilli0520@gmail.com>
2023-05-25 09:12:25 -0500

    Creacion de la clase persona

---
+++ clase.php
@@ -0,0 +1,32 @@
+    <!-- MVC: MODEL VIEW CONTROLLER (PATRON DE DISEÑO) 
+
+-MODEL: Representacion logica de los datos
+-VIEW: Representacion grafica (Frotend)
+-CONTROLLER: Negocio (elementos que nos va permitir interacturar con lainformacion)-->
+
+<?php 
+    class Persona {
+        private $nombre;
+        protected $edad;
+
+        public function __construct ($nombre, $edad) {
+            $this->nombre = $nombre;
+            $this->edad = $edad;
+        }
+        public function getNombre () {
+            return $this->nombre;
+        }
+        public function setNombre ($nombre) {
+            $this->nombre = $nombre;
+        }
+        public function getEdad () {
+            return $this -> edad;
+        }
+        public function setEdad ($nombre){
+            $this->edad = $edad;
+        }
+        public function saludar (){
+            echo "Hola, mi nombre es: " . $this->nombre;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-    <!-- MVC: MODEL VIEW CONTROLLER (PATRON DE DISEÑO) 
-
--MODEL: Representacion logica de los datos
--VIEW: Representacion grafica (Frotend)
--CONTROLLER: Negocio (elementos que nos va permitir interacturar con lainformacion)-->
-
-<?php 
+<?php
+    require_once('clase.php');
+    $alumno = new Persona('Jose Manuel', 17);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <pre class="resultado">
+            <?php echo $alumno->getNombre(); ?>
+            <?php echo $alumno->getEdad(); ?>
+            <?php echo $alumno->saludar(); ?>
+        </pre>  
+    </main>
+</body>
+</html>

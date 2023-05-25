34bd71944c3c943476c7699241f03bf6cabd03cc
Angel Sierra <ancilli0520@gmail.com>
2023-05-25 08:35:01 -0500

    comentario

---
+++ index.php
@@ -0,0 +1,8 @@
+    <!-- MVC: MODEL VIEW CONTROLLER (PATRON DE DISEÑO) 
+
+-MODEL: Representacion logica de los datos
+-VIEW: Representacion grafica (Frotend)
+-CONTROLLER: Negocio (elementos que nos va permitir interacturar con lainformacion)-->
+
+<?php 
+?>
